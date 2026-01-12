0960e49806d38a52b6703494468237a07f8559f1
KISHAN1506 <kishanagarwal1506@gmail.com>
2026-01-12 23:59:56 +0530

    odd even

---
+++ 010_Bit_Manipulation/02_even_odd.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    if((n&1) == 1){
+        cout<<"Odd";
+    }else{
+        cout<<"even";
+    }
+    return 0;
+}
